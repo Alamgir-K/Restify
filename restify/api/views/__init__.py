68ef2dbc563106913bc73b4275c8a821e7a1e8ed
Mixiati Aerzuo <mixiati.aerzuo@mail.utoronto.ca>
2023-03-14 00:49:42 -0400

    changes

--- restify/api/views/__init__.py
+++ restify/api/views/__init__.py
@@ -1,3 +1,3 @@
 from .user import UserSignUpView, UserLoginView, UserLogoutView, UserProfileView
-from .property import PropertyCreateView, PropertyDeleteView, PropertyEditView
+from .property import PropertyCreateView, PropertyDeleteView, PropertyEditView, PropertySearchView
 from .reservation import CreateReservationView, DeleteReservationView, EditReservationView, ReservationDetailView, PropertyReservationsView, UserRequestsView, ReservationRequestsView, RequestNextView, CreateRequestView, ReservationNextView, AllHostRequestsView, AllHostReservationsView
