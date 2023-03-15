d7639c7474d4dd1a4d154d6ea7a3aa4bfcad50d9
Alamgir Khan <k.alamgir2002@gmail.com>
2023-03-15 02:01:36 -0400

    quick fix to related name error

--- restify/api/models/comments.py
+++ restify/api/models/comments.py
@@ -4,20 +4,27 @@ from .reservation import Reservation
 from django.core.validators import MaxValueValidator, MinValueValidator
 from . import CustomUser
 
+
 class UserRating(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='ratings_given', default=None)
-    host = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='ratings_given', default=None)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=None)
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='user_rating', default=None)
+    host = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='host_rating', default=None)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)], default=None)
     comment = models.TextField(default=None)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class CommentChain(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None)
-    host = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments', default=None)
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, default=None)
+    host = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='comments', default=None)
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
     Rating = models.IntegerField(default=None)
     comment = models.TextField()
     hostresponse = models.TextField(default=None, null=True, blank=True)
     userresponse = models.TextField(default=None, null=True, blank=True)
-
