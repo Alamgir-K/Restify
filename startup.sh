75c27d1a1d06a8aca0d0d0a3fba74b16835da3cc
arzumi <91103017+arzumi@users.noreply.github.com>
2023-04-14 16:48:14 -0400

    Update startup.sh
    minor changes to adapt to the new structure

--- startup.sh
+++ startup.sh
@@ -6,9 +6,10 @@ sudo apt-get install -y python3 python3-pip
 python3 -m venv env
 source env/bin/activate
 
-pip3 install -r restify/requirements.txt
+pip3 install -r backend/requirements.txt
 
-python3 restify/manage.py makemigrations
-python3 restify/manage.py migrate
+python3 backend/manage.py makemigrations
+python3 backend/manage.py migrate
+python3 backend/manage.py loaddata db.json
 
 
