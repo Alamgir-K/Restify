e8a103e4c215bfafb533587ab2b869790b3e58fb
Alamgir Khan <k.alamgir2002@gmail.com>
2023-04-23 18:57:17 -0400

    updated script to run frontend

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Django Backend Setup
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 
@@ -12,4 +13,7 @@ python3 backend/manage.py makemigrations
 python3 backend/manage.py migrate
 python3 backend/manage.py loaddata backend/db.json
 
+# React Frontend Setup
+sudo apt-get install -y nodejs npm
 
+npm install --prefix frontend/restify
